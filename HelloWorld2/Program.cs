912d2dc73b32ff54a6dc72a0421a1386eb87ee66
wllee2005 <lee_wee_liam@hotmail.com>
2021-02-24 14:17:35 +0800

    Hello World 2

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("William Lee");
+            Console.WriteLine("William Lee");
+        }
+    }
+}
