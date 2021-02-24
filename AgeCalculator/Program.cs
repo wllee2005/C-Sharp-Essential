19433a6ae510d72acfdf45c41a71ff41c0751834
wllee2005 <lee_wee_liam@hotmail.com>
2021-02-24 17:01:25 +0800

    new project age calculator

---
+++ AgeCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AgeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Please enter your Age: ");
+            string age = Console.ReadLine();
+            
+            Console.WriteLine($"Your Name is {name} and Age is : {age} ");
+        }
+    }
+}
