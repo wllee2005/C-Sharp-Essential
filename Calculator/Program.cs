29d3fd74ce377eaf61fc2d148144f2ddbc8d3c65
wllee2005 <lee_wee_liam@hotmail.com>
2021-03-05 10:55:29 +0800

    add new Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first number: ");
+            string number1 = Console.ReadLine();
+            Console.WriteLine("Please enter your second number: ");
+            string number2 = Console.ReadLine();
+
+            Add(int.Parse(number1), int.Parse(number2));
+            //Add(double.Parse("1"), 2); // convert string to double
+            //Add(1, 2);
+        }
+            
+            public static void Add(double a, double b)
+        {
+            Console.WriteLine($"The sum of {a} and {b} is {a + b}");
+           
+        }
+    }
+}
