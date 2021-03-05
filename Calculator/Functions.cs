5c06a7a3c10d66fa0687cdf5923b57eb729b9516
wllee2005 <lee_wee_liam@hotmail.com>
2021-03-05 15:22:12 +0800

    new Functions added to Calculator

---
+++ Calculator/Functions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Functions
+    {
+        //Fields
+        private double _num1;
+        private double _num2;
+        public Functions(double a, double b) // aka Constructor
+        {   _num1 = a;
+            _num2 = b;
+        }
+
+        public string Add(double a, double b) 
+        {
+            return $"The sum of {a} and {b} is {Math.Round(a + b, 2)}.\n And the values of _num1 and _num2 are {_num1} and {_num2}";
+        }
+        public string Subtract(double a, double b) 
+        {
+            return $"The subtraction of {a} and {b} is {Math.Round(a - b, 2)}.\n And the values of _num1 and _num2 are {_num1} and {_num2}";
+        }
+    }
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,19 +11,35 @@ namespace Calculator
             Console.WriteLine("Please enter your second number: ");
             string number2 = Console.ReadLine();
 
-            double result = Add(double.Parse(number1), double.Parse(number2));
-            Console.WriteLine(result);
+            string result1 = Add(double.Parse(number1), double.Parse(number2));
+            Console.WriteLine(result1);
+
+            string result2 = Subtract(double.Parse(number1), double.Parse(number2));
+            Console.WriteLine(result2);
 
             //Console.WriteLine(Add(double.Parse(number1), double.Parse(number2)));
             //Add(double.Parse("1"), 2); // convert string to double
             //Add(1, 2);
+
+            Functions myFunctions = new Functions(9, 10); // call Functions from Functions.cs with input 2 & 3
+            Console.WriteLine(myFunctions.Add(2, 3));
+            Console.WriteLine(myFunctions.Subtract(2, 3));
+
+            Functions myFunctions2 = new Functions(9, 10); // call Functions from Functions.cs with input 3 & 6
+            Console.WriteLine(myFunctions2.Add(3, 6));
+            Console.WriteLine(myFunctions2.Subtract(3, 6));
         }
-            
-            public static double Add(double a, double b) // void changed to double since there's a return
+
+        public static string Add(double a, double b)
         {
-            //Console.WriteLine($"The sum of {a} and {b} is {a + b}");
-            //Console.WriteLine($"The sum of {a} and {b} is {Math.Round(a + b, 2)}");
-            return Math.Round(a + b, 2); // return a double
+            return $"The sum of {a} and {b} is {Math.Round(a + b, 2)}.";
         }
+        public static string Subtract(double a, double b)
+        {
+            return $"The subtraction of {a} and {b} is {Math.Round(a - b, 2)}.";
+        }
+
+
+
     }
 }
