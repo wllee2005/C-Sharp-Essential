139d97199b7397b197736e70c98587aa4e0dae6e
wllee2005 <lee_wee_liam@hotmail.com>
2021-03-05 16:41:31 +0800

    new Box

---
+++ Box/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+
+    public class Box
+    {
+        double _width;
+        double _length;
+        double _height;
+
+        private static int _counter = 0;
+
+        public Box(double length, double width, double height)
+        {
+            _length = length;
+            _width = width;
+            _height = height;
+
+            _counter++; //_counter = _counter + 1
+
+            Console.WriteLine($"A box of dimensions {_length}, {_width}, {_height} has been created.");
+            Console.WriteLine($"The number of boxes created is {_counter}.");
+        }
+
+        public Box(double length) // Polymorphism
+        {
+            _width = _length = _height = length;
+            _counter++;
+
+            Console.WriteLine($"A box of dimensions {_length}, {_width}, {_height} has been created.");
+            Console.WriteLine($"The number of boxes created is {_counter}.");
+        }
+            public double getVolume()
+        {
+            return _length * _width * _height;
+        }
+    }
+
+}
---
+++ Box/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Box
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box Box1 = new Box(1, 2, 3);
+            Console.WriteLine($"The volume of the box is {Box1.getVolume()}.");
+
+            Box Box2 = new Box(3);
+            Console.WriteLine($"The volume of the box is  {Box2.getVolume()}");
+        }
+    }
+}
