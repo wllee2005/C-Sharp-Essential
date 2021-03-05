5be8c23168346e0b19e92be62744d0334c214694
wllee2005 <lee_wee_liam@hotmail.com>
2021-03-05 11:51:39 +0800

    update to Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,15 +11,19 @@ namespace Calculator
             Console.WriteLine("Please enter your second number: ");
             string number2 = Console.ReadLine();
 
-            Add(int.Parse(number1), int.Parse(number2));
+            double result = Add(double.Parse(number1), double.Parse(number2));
+            Console.WriteLine(result);
+
+            //Console.WriteLine(Add(double.Parse(number1), double.Parse(number2)));
             //Add(double.Parse("1"), 2); // convert string to double
             //Add(1, 2);
         }
             
-            public static void Add(double a, double b)
+            public static double Add(double a, double b) // void changed to double since there's a return
         {
-            Console.WriteLine($"The sum of {a} and {b} is {a + b}");
-           
+            //Console.WriteLine($"The sum of {a} and {b} is {a + b}");
+            //Console.WriteLine($"The sum of {a} and {b} is {Math.Round(a + b, 2)}");
+            return Math.Round(a + b, 2); // return a double
         }
     }
 }
